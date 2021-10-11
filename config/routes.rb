c27271b9f322072d38ab94c749a411d46b0c100f
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-10-11 11:05:12 +0900

    [ActionCable導入]

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,5 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   put "/users/:id/hide" => "users#hide", as: 'users_hide'
+  resources :rooms
 end
