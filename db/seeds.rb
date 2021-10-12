69bc83fe053783e28d4a9a30fc75c5f8573d4a4e
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-10-12 10:42:17 +0900

    [Fakergem導入]

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,26 @@ Tag.create!([
 User.create!(
     email:"n1@n1",
     password: "111111",
-    name: "ドッグさん"
+    name: "ニイジマ",
+    opinion: "ポメラニアンを飼ってます。毛がふさふさでかわいいです。"
 )
 
-50.times do |n|
+25.times do |n|
+    kanji_name = Gimei.kanji
     User.create!(
         email:"test#{n + 1}@test.com",
         password: "111111",
-        name: "山田太郎",
+        name: kanji_name,
         opinion: "柴犬を飼ってます！人懐っこくてすごくかわいいです"
     )
     end
 
-15.times do |n|
-    Dog.create!(
-        user_id: "#{n+1}",
-        name:"ポメラニアン",
-        caption: "柴犬のトイレのしつけ方法を教えてください。"
-    )
-    end
+    # 15.times do |n|
+    # Dog.create!(
+    #     user_id: "#{n+1}",
+    #     name:"ポメラニアン",
+    #     caption: "柴犬のトイレのしつけ方法を教えてください。"
+    # )
+    # end
+    
+    ##Dogモデルの作成はUserモデルの作成を行ってから行う
